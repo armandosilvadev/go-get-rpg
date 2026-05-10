efc3ba533b6aae67511dc4f874fb26e5892028b3
argson <228495808+armandosilvadev@users.noreply.github.com>
2026-05-10 13:12:23 -0300

    update

--- backend/go-get-rpg/src/main/java/dev/armando/go_get_rpg/character/CharacterService.java
+++ backend/go-get-rpg/src/main/java/dev/armando/go_get_rpg/character/CharacterService.java
@@ -1,7 +1,6 @@
 package dev.armando.go_get_rpg.character;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
--- backend/go-get-rpg/src/main/java/dev/armando/go_get_rpg/monster/MonsterController.java
+++ backend/go-get-rpg/src/main/java/dev/armando/go_get_rpg/monster/MonsterController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/monster")
 public class MonsterController {
@@ -14,14 +15,12 @@ public class MonsterController {
     private MonsterService monsterService;
 
     // Get The Monster by its ID
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/{id}")
     public ResponseEntity<MonsterResponseDTO> getMonsterById(@PathVariable String id) {
         return ResponseEntity.ok(monsterService.getMonsterById(id));
     }
 
     // Delete the monster by its ID
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMonsterById(@PathVariable String id) {
         monsterService.deleteMonsterById(id);
@@ -30,14 +29,12 @@ public class MonsterController {
     }
 
     // Get all monsters
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<MonsterResponseDTO> getAllMonsters() {
         return monsterService.getAllMonsters();
     }
 
     // Delete all monsters
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @DeleteMapping
     public ResponseEntity<Void> deleteAll() {
         monsterService.deleteAllMonsters();
@@ -46,14 +43,12 @@ public class MonsterController {
     }
 
     // Save a new monster
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
     public ResponseEntity<MonsterRequestDTO> saveMonster(@RequestBody MonsterRequestDTO data) {
         return ResponseEntity.ok(monsterService.saveMonster(data));
     }
 
     // Update monster
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PutMapping("/{id}")
     public ResponseEntity<MonsterResponseDTO> updateMonsterById(@PathVariable String id, @RequestBody MonsterRequestDTO data) {
         return ResponseEntity.ok(monsterService.updateMonsterById(id, data));
