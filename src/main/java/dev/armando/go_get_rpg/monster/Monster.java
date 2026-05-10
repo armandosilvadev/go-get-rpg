6f62c25bd1d3e253b20e5ff250cdf0df1897c6bf
argson <228495808+armandosilvadev@users.noreply.github.com>
2026-05-10 10:15:35 -0300

    add setter not available for id

--- src/main/java/dev/armando/go_get_rpg/monster/Monster.java
+++ src/main/java/dev/armando/go_get_rpg/monster/Monster.java
@@ -14,6 +14,7 @@ import org.jspecify.annotations.NonNull;
 public class Monster {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Setter(AccessLevel.NONE)
     private String id;
     private String name;
     private double hp;
